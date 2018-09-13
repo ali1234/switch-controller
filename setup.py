afbd1a7655c2c30d572130e4329704b5f66ecf4e
Alistair Buxton <a.j.buxton@gmail.com>
2018-09-13 19:29:23 +0100

    Rename bridge to switchcon and add LICENSE and README.md

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='switchcon',
+    keywords='linux usb gadget functionfs hid',
+    version='0.1',
+    author='Alistair Buxton',
+    author_email='a.j.buxton@gmail.com',
+    url='http://github.com/ali1234/switch-controller',
+    license='GPLv3+',
+    platforms=['linux'],
+    packages=['switchcon'],
+    install_requires=[
+        'functionfs', 'libaio',
+    ],
+    entry_points={
+        'console_scripts': [
+            'switchcon = switchcon.__main__:main'
+        ]
+    },
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Hardware :: Hardware Drivers',
+    ],
+)
--- switchcon/__main__.py
+++ switchcon/__main__.py
@@ -1,4 +1,18 @@
-#!/usr/bin/env python3
+# This file is part of switchcon
+# Copyright (C) 2018  Alistair Buxton <a.j.buxton@gmail.com>
+#
+# switchcon is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# switchcon is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with switchcon.  If not, see <http://www.gnu.org/licenses/>.
 
 
 import argparse
