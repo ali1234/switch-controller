c951acd3c84edc80df5af065c2cf8cf5c134c779
Alistair Buxton <a.j.buxton@gmail.com>
2018-09-21 03:37:24 +0100

    Allow multiple macros to play at once
    This code is a bit rough and should be cleaned up but it appears
    to work.

--- switchcon/macromanager.py
+++ switchcon/macromanager.py
@@ -23,6 +23,7 @@ from .state import State
 
 logger = logging.getLogger(__name__)
 
+
 class MacroManager(object):
     def __init__(self, states, macros_dir='.', record_button=0, play_button=1, function_macros={}):
         self.states = states
@@ -30,8 +31,7 @@ class MacroManager(object):
         self.recordmacro = None
         self.recordfile = None
 
-        self.playmacro = None
-        self.playiter = None
+        self.playing_macros = {}
 
         self.record_button = record_button
         self.play_button = play_button
@@ -49,22 +49,19 @@ class MacroManager(object):
 
     def __exit__(self, *args):
         self.record_stop()
-        self.play_stop()
 
     def log_macro_event(self, event, macro):
-        name = str(macro)
-        try:
-            name = macro.name
-        except AttributeError:
-            pass
-        logger.info('{:s} macro {:s}'.format(
-                event, name
-        ))
+        if callable(macro):
+            logger.info('{:s} macro {:s}'.format(
+                    event, macro.__name__
+            ))
+        else:
+            logger.info('{:s} macro {:s}'.format(
+                    event, str(macro)
+            ))
 
     def record_start(self, macro):
         if self.recordfile is None:
-            if self.playmacro == macro:
-                self.play_stop()
             self.recordmacro = macro
             self.recordfile = macro.open('wb')
             self.log_macro_event('Recording to', macro)
@@ -80,29 +77,38 @@ class MacroManager(object):
             self.log_macro_event('Stopped recording to', self.recordmacro)
             self.recordmacro = None
 
-    def play_start(self, macro):
-        if self.recordmacro == macro:
-            self.record_stop()
-            return
-        if self.playiter is not None:
-            if self.playmacro == macro:
-                self.play_stop()
-                return
-            else:
-                self.play_stop()
-        self.playmacro = macro
-        self.playiter = file(macro)
-        self.log_macro_event('Playing', self.playmacro)
+    def recorder_control(self, record):
+        macro_name = self.previous_state.hexstr[:6] + '.macro'
+        macro_file = self.macros_dir / macro_name
 
-    def play_stop(self):
-        if self.playiter is not None:
-            try:
-                self.playiter.close()
-            except AttributeError:
-                pass
-            self.playiter = None
-            self.log_macro_event('Stopped playing', self.playmacro)
-            self.playmacro = None
+        if record:
+            if self.recordmacro:
+                self.record_stop()
+            else:
+                self.record_start(macro_file)
+        else:
+            if not macro_file.exists():
+                logger.error('Macro not recorded yet.')
+            elif macro_file.is_file():
+                self.play_control(macro_file)
+            else:
+                raise FileNotFoundError("Macro file exists but isn't a regular file.")
+
+    def play_control(self, macro):
+
+        if macro in self.playing_macros:
+            if callable(macro):
+                self.log_macro_event('Stopped playing', macro.__name__)
+            else:
+                self.log_macro_event('Stopped playing', macro)
+            del self.playing_macros[macro]
+        else:
+            if callable(macro):
+                self.log_macro_event('Playing', macro.__name__)
+                self.playing_macros[macro] = macro()
+            else:
+                self.log_macro_event('Playing', macro)
+                self.playing_macros[macro] = file(macro)
 
     def key_event(self, key, down):
         if key >= sdl2.SDLK_0 and key <= sdl2.SDLK_9:
@@ -117,7 +123,6 @@ class MacroManager(object):
                 self.recorder_control(False)
 
 
-
     def button_event(self, button, down):
         if down:
             if button == self.record_button:
@@ -126,39 +131,7 @@ class MacroManager(object):
                 self.recorder_control(False)
             else:
                 if button in self.function_macros:
-
-                    if self.playmacro is None:
-                        self.playmacro = self.function_macros[button].__name__
-                        self.log_macro_event('Playing', self.playmacro)
-                        self.playiter = self.function_macros[button]()
-                    elif self.playmacro == self.function_macros[button].__name__:
-                        self.play_stop()
-                    else:
-                        self.play_stop()
-                        self.playmacro = self.function_macros[button].__name__
-                        self.log_macro_event('Playing', self.playmacro)
-                        self.playiter = self.function_macros[button]()
-
-        else:
-            pass
-
-    def recorder_control(self, record):
-
-        if self.recordmacro:
-            self.record_stop()
-        else:
-            macro_name = self.previous_state.hexstr[:6] + '.macro'
-            macro_file = self.macros_dir / macro_name
-
-            if record:
-                self.record_start(macro_file)
-            else:
-                if not macro_file.exists():
-                    logger.error('Macro not recorded yet.')
-                elif macro_file.is_file():
-                    self.play_start(macro_file)
-                else:
-                    raise FileNotFoundError("Macro file exists but isn't a regular file.")
+                    self.play_control(self.function_macros[button])
 
     def __iter__(self):
         return self
@@ -166,9 +139,9 @@ class MacroManager(object):
     def __next__(self):
         n = next(self.states)
         self.previous_state = n
-        if self.playiter is not None:
+        for macro, gen in list(self.playing_macros.items()):
             try:
-                m = next(self.playiter)
+                m = next(gen)
                 # OR buttons
                 n |= (m&State(0xffff, 0x00, 0, 0, 0, 0))
                 # user hat overrides macro hat if not centred
@@ -176,7 +149,11 @@ class MacroManager(object):
                 # user axes override macro axes if > 50%
                 n.axes = [na if (na < 64) or (na > 192) else ma for na, ma in zip(n.axes, m.axes)]
             except StopIteration:
-                self.play_stop()
+                if callable(macro):
+                    self.log_macro_event('Stopped playing', macro.__name__)
+                else:
+                    self.log_macro_event('Stopped playing', macro)
+                del self.playing_macros[macro]
 
         if self.recordfile is not None:
             self.recordfile.write(n.hex + b'\n')
